4e92587f09079328881df435bf957edee1bad49f
Voldie <voldiekami@gmail.com>
2017-11-27 21:51:24 +0100

    Added OpenGL Texture2D object header file.

---
+++ include/Texture2D.h
@@ -0,0 +1,64 @@
+/**
+    Vector field simulation.
+    Copyright (C) 2017  Valdemar Lindberg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef _VF_TEXTURE_H_
+#define _VF_TEXTURE_H_ 1
+#import"Resource.h"
+
+/**
+ * 
+ */
+@interface Texture2D : Resource {
+    unsigned int texture;
+    int width;
+    int height;
+}
+
+/**
+ * Constructor of the object.
+ * @return object id.
+ */
+-(id) initWithTexture: (int) texture: (int) width: (int) height;
+
+/**
+ * Release all resources assoicated
+ * with this object.
+ */
+-(void) release;
+
+/**
+ * Bind texture as current texture
+ * assoicated with the index.
+ */
+-(void) bind: (int) index;
+
+/**
+ * Get the width of the texture.
+ * @return non-negative width.
+ */
+-(int) getWidth;
+
+/**
+ * Get the height of the texutre.
+ * @return non-negative height.
+ */
+-(int) getHeight;
+
+@end
+
+#endif
