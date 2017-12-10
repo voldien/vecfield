9db52b4eb847794ae184377c1b6a5f6a6dc9cccf
Voldie <voldiekami@gmail.com>
2017-12-10 08:33:21 +0100

    Fixed indentation.

--- src/main.m
+++ src/main.m
@@ -2,29 +2,29 @@
 #import<Foundation/Foundation.h>
 
 int main(int argc, const char** argv){
-   
-    @try{
-        
-        /*  Create Vector field and run simulation.    */
-        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-        VecField* field = [VecField createVecField: argc: argv];
-        [field startSimulation];
 
-        /*  Release all resources.  */
-        [pool drain];
+	@try{
 
-    }@catch(NSException* ex){
-        fprintf(stderr, "Error occurred : [%s] - %s.\n", [ [ex name] cString], [[ex reason] cString]);
-        #ifdef _DEBUG
-        NSArray* stack = [NSThread callStackSymbols];
-        stack = [ex callStackSymbols];
-        fprintf(stderr, "\n----------- Stack Trace ---------\n");
-        for(NSString* s in stack){
-            fprintf(stderr, "%s\n", [s cString]);            
-        }
-        #endif
-        return EXIT_FAILURE;
-    }
-        
+		/*  Create Vector field and run simulation.    */
+		NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+		VecField* field = [VecField createVecField: argc: argv];
+		[field startSimulation];
+
+		/*  Release all resources.  */
+		[pool drain];
+
+	}@catch(NSException* ex){
+		fprintf(stderr, "Error occurred : [%s] - %s.\n", [ [ex name] cString], [[ex reason] cString]);
+		#ifdef _DEBUG
+		NSArray* stack = [NSThread callStackSymbols];
+		stack = [ex callStackSymbols];
+		fprintf(stderr, "\n----------- Stack Trace ---------\n");
+		for(NSString* s in stack){
+			fprintf(stderr, "%s\n", [s cString]);
+		}
+		#endif
+		return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
 }
