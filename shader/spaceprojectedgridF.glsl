f7fc510a445f2f127be5a1e3db4ac19bc3c62f21
Voldie <voldiekami@gmail.com>
2018-01-06 12:05:49 +0100

    Changes.

--- shader/spaceprojectedgridF.glsl
+++ shader/spaceprojectedgridF.glsl
@@ -5,6 +5,7 @@ out vec4 fragColor;
 #endif
 
 /*	*/
+uniform sampler2D tex1;
 uniform mat4 view;
 uniform float zoom = 1.0;
 uniform float thick = 10.0205;
@@ -22,44 +23,27 @@ float resultHorVer(const in vec3 pos, float scale){
 	/*	*/
 	const float z = scale;
 	const vec2 invScreen = 1.0f / screen;
-	const vec2 halfThick = (invScreen / 2.0) * thick * scale;
 	
-	/*	*/
-	vec2 p = pos.xy;
-	vec2 dy = ceil(p) - p;
+	vec2 ex_UV = (invScreen * vec2(gl_FragCoord.xy)) * scale + pos.xy ;
 	
-	/*	Compute dotted.	*/
-	const float freq = 1000;
-	const float xdotted = round(cos(gl_FragCoord.x * freq));
-	const float ydotted = round(cos(gl_FragCoord.y * freq));
-	
-	/*	*/
-	bvec2 gr = lessThan(dy, halfThick);
-	
-	/*	*/
-	int s = int(gr.x) * int(xdotted);
-	int b = int(gr.y) * int(ydotted);
-	
-	return (b | s);
+	return texture(tex1, ex_UV).a;
 }
 
 /*  Compute the grid color. */
 vec4 computeColor(){
 
 	/*	constants.	*/
-	const float grid1Intensity = 0.8;
+	const float grid1Intensity = 0.75;
 	const float grid2Intensity = 0.45;
 
 	/*	*/
-	const float zscale = 1.0f / zoom;
+	const float zscale = (68.0 - zoom);
 	const vec3 pos1 = (view * vec4(vVertex, 0.0)).xyz;
 	const vec3 pos2 = (view * vec4(vVertex, 0.0)).xyz + (view * vec4(0.5,0.5,0,0)).xyz;
 
 	/*  Compute grid color.   */
-	float color = resultHorVer(pos1, abs(zscale)) * grid1Intensity;
-	color += resultHorVer(pos2, zoom) * round( cos(pos2.x)) * grid2Intensity;
-
-	
+	float color = resultHorVer(pos1, zscale) * grid1Intensity;
+	color += resultHorVer(pos2, zscale)* grid2Intensity;
 	/*  Final color.	*/
 	return vec4(color, color, color, 1.0);
 }
